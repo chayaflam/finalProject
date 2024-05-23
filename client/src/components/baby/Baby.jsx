cbf207536c728abeb62b9a3b04e1aba76d3e3a6b
ruthSalomon <ruth32608@gmail.com>
2024-05-23 21:30:29 +0300

    🥱😷🤕🤮🤧

--- client/src/components/baby/Baby.jsx
+++ client/src/components/baby/Baby.jsx
@@ -1,7 +1,26 @@
 import React, { useEffect, useContext } from "react";
+import { useForm } from 'react-hook-form';
+import io from 'socket.io-client'
 
 export default function Baby() {
+    const URL = "http://localhost:8080"
+    const { register, handleSubmit } = useForm();
+
+    let socket = io.connect(`${URL}`, {
+        reconnection: false
+    });
+
+    const teacherMessageHandleSubmit = (data) => {
+        socket.on('connect', function() {
+            console.log('Connected to server');
+            socket.emit( 'data from client',data);
+        });
+    }
+
     return (<>
-        <h1>baby!!!</h1>
+         <form onSubmit={handleSubmit(teacherMessageHandleSubmit)}>
+         <label>message:<textarea type="text" name="teacherMessage" {...register("teacherMessage")} /></label><br />
+        <input type="submit" value="Submit" /><br />
+        </form>
     </>)
 };
--- server/app.js
+++ server/app.js
@@ -4,9 +4,26 @@ import { userRouter } from './Router/userRouter.js';
 import { authRouter } from './Router/authRouter.js';
 import {authMiddleWare} from './MiddleWare/authMiddleWare.js';
 import { logErrors } from './MiddleWare/logError.js';
+import { io } from "socket.io-client";
+
+const socket = io();
+
+export default socket;
 
 const app = express();
+io.on("connection", (socket) => {
+    console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
+  });
+// io.listen(process.env.PORT||5000);
+// io.on('connection', function(socket) {
+//     console.log('client connected');
 
+//     // listen for incoming data msg on this newly connected socket
+//     socket.on('data',function (data) {
+//         console.log(`data received is '${data}'`)
+//     });
+
+// });
 app.use(cors())
 app.use(express.json());
 app.use('/auth', authRouter)
@@ -18,3 +35,8 @@ app.listen(8080, (err) => {
     if (err) console.error(err);
     console.log("Server listening on PORT", 8080);
 });
+
+
+
+
+
