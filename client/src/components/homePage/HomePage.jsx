edfd11870a2503df3ad7d9b3a9e6a6e69261c95c
ruthSalomon <ruth32608@gmail.com>
2024-06-24 23:19:44 +0300

    nice

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import 'primeicons/primeicons.css';
-
+import './HomePage.css'
 export default function HomePage() {
 
     return (<>
         <NavLink to="/login" icon="pi pi-user">Login</NavLink>
-        <img className="homePageImg" src="../../../public/img/homePage.jpg" />
+        <img className="homePageImg" src="../../../public/img/homePage.jpeg" />
     </>)
 };
